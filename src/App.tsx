1f512b817692e9ff914938b00718b34fcbcc1c1b
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-10 11:44:13 -0300

    feat: character movimentando para todos os lados

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import * as C from './App.styles';
 import { Character } from './components/Character';
 import { useCharacter } from './hooks/useCharacter';
@@ -5,6 +6,33 @@ import { useCharacter } from './hooks/useCharacter';
 const App = () => {
 
   const char = useCharacter();
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      
+      break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+
+      break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+
+      break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+    }
+  }
 
   return (
     <C.Container>
