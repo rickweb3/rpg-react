4a0e524310c76cc8385c6a7cd8a3e3bc83b25406
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-10 12:19:24 -0300

    feat: exibindo imagem do character de acordo com o lado de sua movimentação

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
   return (
     <C.Container>
       <C.Map>
-        <Character x={char.x} y={char.y}/>
+        <Character x={char.x} y={char.y} side={char.side}/>
       </C.Map>
     </C.Container>
   );
